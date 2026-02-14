8f67daa2dd1876c1e2019c2cda58adff1520807f
Pratik Gupta <pratikgupta1335@gmail.com>
2026-02-15 00:40:19 +0530

    Header Part done

--- Angular_Concepts_01/src/app/app.ts
+++ Angular_Concepts_01/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Angular_Concepts_01');
 }
