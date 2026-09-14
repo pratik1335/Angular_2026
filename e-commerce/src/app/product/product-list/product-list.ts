9ad429e2ca4f1969658b9f989debbffdc923e6bd
Pratik Gupta <pratikgupta1335@gmail.com>
2026-09-14 21:17:45 +0530

    Add Angular material Components

--- e-commerce/src/app/product/product-list/product-list.ts
+++ e-commerce/src/app/product/product-list/product-list.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product-service';
 import { Product } from '../../models/product';
+import { MatCardModule } from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [MatCardModule, CurrencyPipe],
   selector: 'app-product-list',
   styleUrl: './product-list.css',
   templateUrl: './product-list.html',
