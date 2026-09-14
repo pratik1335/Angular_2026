ee32ad937671b1cdac97eed9f86aeb3d20b0d271
Pratik Gupta <pratikgupta1335@gmail.com>
2026-09-14 14:02:47 +0530

    display the products data

--- e-commerce/src/app/product/product-list/product-list.ts
+++ e-commerce/src/app/product/product-list/product-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product-service';
+import { Product } from '../../models/product';
 
 @Component({
   imports: [],
@@ -6,4 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.css',
   templateUrl: './product-list.html',
 })
-export class ProductList {}
+export class ProductList implements OnInit{
+
+  /*
+    Now we want to get some products right.
+    If we want to share the products that we will get just in a second with our HTML template, 
+    we have to create a property here and share it using the interpolation.
+    So we create the property which we call products.
+  */
+  products : Product[] = [];
+
+  /*
+    we first of all need an instance of our product service.
+    So let's create a constructor to make use of the dependency injection 
+    and can do that by creating a private product service here of type product service.
+  */
+  constructor(private productService : ProductService){}
+
+  /*
+    First of all, we have to think about when do we want to call that method?
+    So at which point of our application and usually what we want to do here is 
+    as soon as the component renders, right, we already want to have that data or fetch that data.
+    So therefore we use the oninit lifecycle hook that you have already learned about.
+  */
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    })
+  }
+}
