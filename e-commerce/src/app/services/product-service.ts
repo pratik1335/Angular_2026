3ffcd541f2eaa49d92c21eee3462f21bf645b7fa
Pratik Gupta <pratikgupta1335@gmail.com>
2026-09-14 13:27:30 +0530

    get products using mock api

--- e-commerce/src/app/services/product-service.ts
+++ e-commerce/src/app/services/product-service.ts
@@ -1,4 +1,21 @@
-import { Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
-@Service()
-export class ProductService {}
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ProductService {
+
+    private apiUrl = environment.apiUrl + '/products';  // "http://localhost:3000/products"
+
+    constructor(private http : HttpClient){}
+
+    getProducts() : Observable<Product[]>{
+        return this.http.get<Product[]>(this.apiUrl)
+    }
+
+}
--- e-commerce/src/environments/environment.ts
+++ e-commerce/src/environments/environment.ts
@@ -4,3 +4,11 @@
 export const environment = {
     apiUrl: '',
 };
+
+/*  
+    We could also do that for production later on, add API URL, but change that one right, 
+    Change it to the production URL and our code would simply pick the correct environment file 
+    which we have specified in our angular Json and then read the correct value 
+    without the need of switching the code from our site.
+
+*/
