6e4b21b2d88471ec33eb3d9708c0d848ba91574c
Pratik Gupta <pratikgupta1335@gmail.com>
2026-09-18 21:34:28 +0530

    Create service methods

--- e-commerce/src/app/services/cart-service.ts
+++ e-commerce/src/app/services/cart-service.ts
@@ -1,4 +1,27 @@
-import { Service } from '@angular/core';
+import { Injectable, Service } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product';
+import { Observable } from 'rxjs';
+@Injectable({
+    providedIn: 'root'
+})
 
-@Service()
-export class CartService {}
+export class CartService {
+
+    apiUrl = environment.apiUrl + '/cart';
+
+    constructor(private http : HttpClient){}
+
+    addCart(product : Product) : Observable<Product>{
+        return this.http.post<Product>(this.apiUrl, product);
+    }
+
+    getCartItems() : Observable<Product[]>{
+        return this.http.get<Product[]>(this.apiUrl);
+    }
+
+    removeCart() : Observable<void>{
+        return this.http.delete<void>(this.apiUrl)
+    }
+}
