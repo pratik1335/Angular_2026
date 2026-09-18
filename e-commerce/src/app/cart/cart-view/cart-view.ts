335a4f1da61ac1a9b6ec297023e7bee74427770a
Pratik Gupta <pratikgupta1335@gmail.com>
2026-09-18 21:56:10 +0530

    Get cart items & use mat-list for cart items

--- e-commerce/src/app/cart/cart-view/cart-view.ts
+++ e-commerce/src/app/cart/cart-view/cart-view.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart-service';
+import { Product } from '../../models/product';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatListModule, CurrencyPipe],
   selector: 'app-cart-view',
   styleUrl: './cart-view.css',
   templateUrl: './cart-view.html',
 })
-export class CartView {}
+export class CartView implements OnInit {
+
+  cartItems : Product[] = [];
+
+  constructor(private cartService : CartService){}
+
+  ngOnInit() : void{
+    this.cartService.getCartItems().subscribe(data => {
+      this.cartItems = data;
+    })
+  }
+}
