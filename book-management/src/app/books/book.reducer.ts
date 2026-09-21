480e503fba9f7f5a58a98a5bef00a0cda7981931
Pratik Gupta <pratikgupta1335@gmail.com>
2026-09-21 23:54:46 +0530

    create the book reducer

---
+++ book-management/src/app/books/book.reducer.ts
@@ -0,0 +1,27 @@
+import { createReducer, on } from "@ngrx/store";
+import { AddBook, RemoveBook } from "./book.actions";
+import { Book } from "../models/book";
+
+// A reducer always needs an initial state
+export const initialState: ReadonlyArray<Book> = [];
+
+export const BookReducer = createReducer(
+    initialState,
+    on(AddBook, (state, {id, title, author}) => [...state, {id, title, author}]),
+    on(RemoveBook, (state, {bookId}) => state.filter(book => book.id !== bookId))
+)
+
+
+/*
+Finally, the reducer needs to know about the book. Why does the reducer needs to know about the book?
+A reducer only takes care of a segment of the app state.
+For example: app state may have book, users so then we will have
+separate reducers for book as well as for the users
+*/
+
+/*
+We use reducers that process actions to change the state. But how does that work in the end?
+And the way a reducer is changing the state is by copying the current state, 
+then making changes and returning a new state, right?
+So we take the state, we copy it, make changes, return it.
+*/
